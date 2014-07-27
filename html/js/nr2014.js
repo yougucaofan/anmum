f7608c5b585c7a9d2a5a06de7b5dbf1c22a235b2
smallgrass <yougucaofan@126.com>
2014-07-27 20:16:36 +0800

    over

--- html/js/nr2014.js
+++ html/js/nr2014.js
@@ -35,9 +35,87 @@ function faqSlide(){
 	})
 }
 
+// 分享页面用户反馈
+function clientFeedBack() {
+	var elem = $('#slide-feedback');
+	if(!elem.length) return;
+	var pre = elem.find('.pre'),
+		next = elem.find('.next'),
+		moveElem = elem.find('ul'),
+		lis = elem.find('li'),
+		ulContent = moveElem.html(),
+		count = 0,
+		len = lis.length,
+		interval = 3000,
+		w = lis.outerWidth(true),
+		defaultLeft = -len * w,
+		setInterval_elem, dis;
+
+	// 初始化设置ul内容复制等
+	_init();
+	setInterval_elem = setInterval(_move, 3000)
+
+	// 绑定事件
+	next.click(function(){
+		if(moveElem.is(':animated')) return;
+		_move();
+	});
+	pre.click(function(){
+		if(moveElem.is(':animated')) return;
+		count = count -2;
+		_move();
+	});
+	elem.hover(function(e){
+		setInterval_elem && clearInterval(setInterval_elem);
+		if(e.type == 'mouseleave') {
+			setInterval_elem = setInterval(_move, interval);
+		}
+	})
+
+	// 事件函数
+	function _move(){
+		 ++count;
+		if(count < 0){
+			moveElem.css('margin-left', 2*defaultLeft);
+			count = len - 1;
+		} else if(count > len) {
+			count = 1;
+			moveElem.css('margin-left', defaultLeft);
+		};
+
+		dis = defaultLeft - count*w;
+		moveElem.animate({marginLeft: dis});
+	};
+	function _init() {
+		moveElem.css('margin-left', defaultLeft).html(ulContent + ulContent + ulContent);
+	}
+}
+
+// 模拟select
+function selectMod() {
+	var elem = $('.nr-select .cell');
+	if(!elem.length) return;
+	elem.each(function(){
+		var self = $(this),
+			handle = self.find('.val'),
+			handlCon = handle.find('span'),
+			list = self.find('.list'),
+			lis = list.find('li');
+		handle.click(function(){
+			list.toggle();
+		});
+		lis.click(function(){
+			handlCon.text($(this).text());
+			list.hide();
+		});
+	})
+}
+
 
 
 
 $(function(){
 	faqSlide();
+	selectMod();
+	clientFeedBack();
 })
